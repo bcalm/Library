dcf78416dbb4cc86e79bc34d73771f016e7a3248
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2022-10-11 16:11:57 +0530

    Added OAuth flow via github

--- src/main/java/com/library/config/SecurityConfig.java
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -1,23 +1,10 @@
 package com.library.config;
 
-import com.library.filter.AuthoritiesLoggingAfterFilter;
-import com.library.filter.AuthoritiesLoggingAtFilter;
-import com.library.filter.JWTTokenGeneratorFilter;
-import com.library.filter.JWTTokenValidatorFilter;
-import com.library.filter.RequestValidationBeforeFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import static java.util.Collections.singletonList;
 
 @Configuration
 @Slf4j
@@ -26,35 +13,41 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .cors().configurationSource(request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(singletonList("http://localhost:4200"));
-                config.setAllowedMethods(singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(singletonList("*"));
-                config.setExposedHeaders(singletonList("Authorization"));
-                config.setMaxAge(3600L);
-                return config;
-            }).and().csrf().disable()
-            .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-            .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
-            .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-            .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
-            .authorizeHttpRequests((auth) -> auth
-                .antMatchers("/addBook").authenticated()
-                .antMatchers("/borrowBook/{bookId}").authenticated()
-                .antMatchers("/library/status").authenticated()
-                .antMatchers("/returnBook/{bookId}").authenticated()
-                .antMatchers("/welcome").authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests((auth) -> {
+            try {
+                auth.anyRequest().authenticated().and().oauth2Login();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            ;
+        });
         return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
+/*private ClientRegistration clientRegistration() {
+		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("42d799aafaa59eee70bf")
+	           .clientSecret("911ac4de96a198377ad2dd94f51707177350683d").build();
+	 }*/
+
+
+/*
+ * private ClientRegistration clientRegistration() { ClientRegistration cr =
+ * ClientRegistration.withRegistrationId("github").clientId(
+ * "3c9be97074f067e78e75")
+ * .clientSecret("ab313f7ade3d79e06c192ca80cf152c43cb5d916").scope(new String[]
+ * { "read:user" })
+ * .authorizationUri("https://github.com/login/oauth/authorize")
+ * .tokenUri("https://github.com/login/oauth/access_token").userInfoUri(
+ * "https://api.github.com/user")
+ * .userNameAttributeName("id").clientName("GitHub")
+ * .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+ * .redirectUriTemplate("{baseUrl}/{action}/oauth2/code/{registrationId}").build
+ * (); return cr; }
+ */
+
+
+/*
+ * @Bean public ClientRegistrationRepository clientRepository() {
+ * ClientRegistration clientReg = clientRegistration(); return new
+ * InMemoryClientRegistrationRepository(clientReg); }
+ */
